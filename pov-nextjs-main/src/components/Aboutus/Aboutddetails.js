aab0aea0c85818cef24cce15d75fa962f3278a2d
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-03-12 22:55:01 +0530

    ff

--- pov-nextjs-main/src/components/Aboutus/Aboutddetails.js
+++ pov-nextjs-main/src/components/Aboutus/Aboutddetails.js
@@ -133,10 +133,10 @@ const Aboutddetails = () => {
         {/* <ul className="pricing-one__list">
           <p className="pricing-one__list-item">Mr. Swapnil Gupta leads our marketing efforts, crafting viral content campaigns and spearheading performance marketing strategies to drive engagement and brand growth.</p>
         </ul> */}
-        <a href="https://www.linkedin.com/in/swapnilguptaa" className="thm-btn pricing-one__btn">
+        {/* <a href="https://www.linkedin.com/in/swapnilguptaa" className="thm-btn pricing-one__btn">
           Contact
         </a>
-        <span className="pricing-one__tag-line">Marketing</span>
+        <span className="pricing-one__tag-line">Marketing</span> */}
       </div>
     </Col>
         <Col lg={4}>
@@ -152,10 +152,10 @@ const Aboutddetails = () => {
         {/* <ul className="pricing-one__list">
           <p className="pricing-one__list-item">As our Creative Head, Ms. Diksha Singh is the driving force behind our innovative campaigns. She leads the team with her creative vision, ensuring each project exceeds expectations.</p>
         </ul> */}
-        <a href="https://www.linkedin.com/in/diksha-singh-037a00203?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="thm-btn pricing-one__btn">
+        {/* <a href="https://www.linkedin.com/in/diksha-singh-037a00203?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="thm-btn pricing-one__btn">
           Contact
         </a>
-        <span className="pricing-one__tag-line">Designing</span>
+        <span className="pricing-one__tag-line">Designing</span> */}
       </div>
     </Col>
         <Col lg={4}>
@@ -171,10 +171,10 @@ const Aboutddetails = () => {
         {/* <ul className="pricing-one__list">
           <p className="pricing-one__list-item">With his expertise in film making and editing Mr. Prakhar Dixit brings storytelling to life. His excellence in visual storytelling enhances our content, captivating audiences and driving results.</p>
         </ul> */}
-        <a href="https://www.linkedin.com/in/prakhar-kumar-dixit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"className="thm-btn pricing-one__btn">
+        {/* <a href="https://www.linkedin.com/in/prakhar-kumar-dixit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"className="thm-btn pricing-one__btn">
           Contact
         </a>
-        <span className="pricing-one__tag-line">Editing</span>
+        <span className="pricing-one__tag-line">Editing</span> */}
       </div>
       
     </Col>
@@ -191,10 +191,10 @@ const Aboutddetails = () => {
         {/* <ul className="pricing-one__list">
           <p className="pricing-one__list-item">With his expertise in film making and editing Mr. Prakhar Dixit brings storytelling to life. His excellence in visual storytelling enhances our content, captivating audiences and driving results.</p>
         </ul> */}
-        <a href="https://www.linkedin.com/in/prakhar-kumar-dixit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"className="thm-btn pricing-one__btn">
+        {/* <a href="https://www.linkedin.com/in/prakhar-kumar-dixit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"className="thm-btn pricing-one__btn">
           Contact
         </a>
-        <span className="pricing-one__tag-line">Editing</span>
+        <span className="pricing-one__tag-line">Editing</span> */}
       </div>
       
     </Col>
@@ -211,10 +211,10 @@ const Aboutddetails = () => {
         {/* <ul className="pricing-one__list">
           <p className="pricing-one__list-item">With his expertise in film making and editing Mr. Prakhar Dixit brings storytelling to life. His excellence in visual storytelling enhances our content, captivating audiences and driving results.</p>
         </ul> */}
-        <a href="https://www.linkedin.com/in/prakhar-kumar-dixit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"className="thm-btn pricing-one__btn">
+        {/* <a href="https://www.linkedin.com/in/prakhar-kumar-dixit?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"className="thm-btn pricing-one__btn">
           Contact
         </a>
-        <span className="pricing-one__tag-line">Editing</span>
+        <span className="pricing-one__tag-line">Editing</span> */}
       </div>
       
     </Col>
