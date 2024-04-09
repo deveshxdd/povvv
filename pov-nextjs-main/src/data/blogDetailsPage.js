73b59c263d1cee115621bb5ea3efbd823c058346
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-04-09 15:18:36 +0530

    dd

--- pov-nextjs-main/src/data/blogDetailsPage.js
+++ pov-nextjs-main/src/data/blogDetailsPage.js
@@ -1,4 +1,4 @@
-import author from "../../public/assets/images/author-1-1.jpg";
+import author from "../../public/assets/images/author-1-1.png";
 import image from "../../public/assets/images/blog-d-1-1.jpg";
 
 const blogDetailsPage = {
@@ -10,39 +10,38 @@ const blogDetailsPage = {
       {
         id: 1,
         image: "comment-1-1.jpg",
-        name: "Laquanda Bachmeier",
-        date: "20 April, 2019",
-        time: "4:00 pm",
-        text: "Lorem Ipsum is simply dummy text of the rinting and typesetting been the industry standard dummy text ever sincer condimentum purus. In non ex at ligula fringilla lobortis et not the aliquet.",
+        name: "devesh upadhyay",
+        date: "09 April, 2024",
+        time: "4:20 pm",
+        text: "Nice blog totally agreed",
       },
       {
         id: 2,
         image: "comment-1-2.jpg",
-        name: "Vicente Elmore",
-        date: "20 April, 2019",
+        name: "nishant",
+        date: "09 April, 2024",
         time: "4:00 pm",
-        text: "Lorem Ipsum is simply dummy text of the rinting and typesetting been the industry standard dummy text ever sincer condimentum purus. In non ex at ligula fringilla lobortis et not the aliquet.",
-      },
+        text: "got the points clearly",}
     ],
     title: "Bhautiki Plus: Spinning Their Way to Social Media Domination (And How You Can Too!)",
     texts: [
-      "There are many variations of passages of available but majority have alteration in some by inject humour or random words. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.",
+      
+     " Remember that mind-bending video where water defied gravity, refusing to spill even when flipped upside down? The internet exploded, and Bhautiki Plus, the Gwalior-based EdTech brand behind it all, was celebrating a 6 million+ impression campaign that swept across social media like wildfire! But how did a seemingly simple physics experiment become a viral sensation? Let's break it down into five key ingredients that made this campaign a social media success story: 1. The Hook: A Trailer, Not a Spoiler: We didn't give away the answer right away. The initial video acted like a trailer, piquing curiosity with the impossible rotation and leaving viewers yearning for more. Imagine scrolling through your feed and seeing water defying gravity – wouldn't you stop, like, and share to see what happens? 2. The Challenge: Embrace the Unexpected: More than just a trick, it was a call to action. We challenged viewers to defy their expectations, to embrace the impossible, just like Bhautiki Plus pushes the boundaries of learning. It wasn't just about the water, it was about sparking a social media frenzy around curiosity and the unknown. 3. Adding Value: Not Just a Gimmick: We didn't stop at the wow factor. The full video delved into the science behind the phenomenon, making it an educational experience disguised as entertainment. It wasn't just a magic trick; it was a chance to learn something new and exciting, and share it with others! 4. Mind-Reading the Audience: We anticipated viewer thoughts. The video addressed potential skepticism, creating a relatable and interactive experience. We knew people might think it was fake, so we addressed those doubts head-on, making the experience more engaging and sparking discussions across social media platforms. 5. The Cliffhanger: Keeping the Conversation Alive: We ended abruptly, leaving viewers wanting more. This sparked engagement, comments, and a community eager to discuss the science behind the magic. We didn't give away all the answers, leaving people curious and wanting to dive deeper, fueling the social media conversation even further. This campaign wasn't just about views; it was about sparking curiosity, igniting imaginations, and proving that learning can be both fun and thought-provoking. Bhautiki Plus successfully used a simple experiment to connect with millions on social media, showcasing the power of creativity and innovation in education Ready to unlock the pote"
     ],
     author: {
-      name: "Christine Eve",
+      name: " Author - Diksha",
       image: author,
-      text: "Lorem Ipsum is simply dummy text of the rinting and typesetting been the industry standard dummy text ever sincer condimentum purus. In non ex at ligula fringilla lobortis et not the aliquet.",
+      text: "Blog - Bhautiki Plus: Spinning Their Way to Social Media Domination (And How You Can Too!)",
     },
   },
   tags: [
     "Business",
     "Agency",
     "Technology",
-    "Parallax",
-    "Innovative",
-    "Professional",
-    "Experience",
+    "Social media",
+    "Ad compaign",
+    "Video production",
+    "Shoot ",
   ],
   share: [
     "fa fa-twitter",
@@ -54,26 +53,26 @@ const blogDetailsPage = {
     {
       id: 1,
       image: "lp-1-1.jpg",
-      title: "Pre launch mobile app marketing pitfalls",
+      title: "how to shoot videos for company",
     },
     {
       id: 2,
-      image: "lp-1-2.jpg",
-      title: "Social currency per- formance keywords or",
+      image: "lp-1-1.jpg",
+      title: "How to run ads",
     },
     {
       id: 3,
-      image: "lp-1-3.jpg",
-      title: "Prioritize these items quarterly sales are at",
+      image: "lp-1-1.jpg",
+      title: "What is content marketing",
     },
   ],
   categories: [
     "Business",
     "Introductions",
-    "One Page Template",
-    "Parallax Effects",
-    "New Technologies",
-    "Video Backgrounds",
+    "Social media ",
+    "Ad compaign",
+    "Content creation",
+    "Video Production",
   ],
 };
 
