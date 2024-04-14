7bb72424dde5816983ba527e951c0d7add968846
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-04-14 15:14:22 +0530

    pp

--- pov-nextjs-main/src/data/brandOne.js
+++ pov-nextjs-main/src/data/brandOne.js
@@ -4,15 +4,23 @@ import image3 from "../../public/assets/images/img3.png";
 import image4 from "../../public/assets/images/img4.png";
 import image5 from "../../public/assets/images/img5.png";
 import image6 from "../../public/assets/images/img6.png";
+import image7 from "../../public/assets/images/new1.png";
+import image8 from "../../public/assets/images/new2.png";
+import image9 from "../../public/assets/images/new3.png";
+import image10 from "../../public/assets/images/new4.png";
 
 
 const brandOne = [
   image1,
   image2,
+  image7,
   image3,
-  image4,
+  image8,
+  image6,
   image5,
-  image6
+  image4,
+  image9,
+  image10
 ];
 
 export default brandOne;
