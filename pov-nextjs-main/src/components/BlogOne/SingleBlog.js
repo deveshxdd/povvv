6de9fd720960f54154d69ba2f51eb377dfefa819
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-04-15 16:32:14 +0530

    pop
    45t4

--- pov-nextjs-main/src/components/BlogOne/SingleBlog.js
+++ pov-nextjs-main/src/components/BlogOne/SingleBlog.js
@@ -4,7 +4,7 @@ import { Col, Image } from "react-bootstrap";
 import TextSplit from "../Reuseable/TextSplit";
 
 const SingleBlog = ({ blog = {} }) => {
-  const { title, image, admin, date, comments } = blog;
+  const { title, image, admin, date, comments , readd , linkk } = blog;
 
   return (
     <Col lg={6}>
@@ -15,29 +15,29 @@ const SingleBlog = ({ blog = {} }) => {
               src={require(`../../../public/assets/images/${image}`).default.src}
               alt="Awesome Image"
             />
-            <Link href="/blog-details">
-              <a className="blog-one__link">Read More</a>
+            <Link href={linkk}>
+              <a className="blog-one__link">{readd}</a>
             </Link>
           </div>
         </div>
 
         <div className="blog-one__content">
           <div className="blog-one__meta">
-            <Link href="/blog-details">
+            <Link href={linkk}>
               <a className="blog-one__meta-link">By {admin}</a>
             </Link>
             <span className="blog-one__meta-sep">.</span>
-            <Link href="/blog-details">
+            <Link href={linkk}>
               <a className="blog-one__meta-link">{date}</a>
             </Link>
             <span className="blog-one__meta-sep">.</span>
-            <Link href="/blog-details">
+            <Link href={linkk}>
               <a className="blog-one__meta-link">{comments} comments</a>
             </Link>
           </div>
 
           <h3 className="blog-one__title">
-            <Link href="/blog-details">
+            <Link href={linkk}>
               <a>
                 <TextSplit text={title} />
               </a>
--- pov-nextjs-main/src/data/blogOne.js
+++ pov-nextjs-main/src/data/blogOne.js
@@ -3,6 +3,8 @@ const blogOne = {
   blogs: [
     {
       id: 1,
+      readd: "Read More",
+      linkk: "/blog-details",
       title: "Bhautiki Plus: Spinning Their Way to Social Media Domination (And How You Can Too!)",
       image: "blog-1-1.jpg",
       admin: "admin",
@@ -11,6 +13,8 @@ const blogOne = {
     },
     {
       id: 2,
+      readd: "Upcoming",
+      linkk: "/blog",
       title: "Garg Classes Shifts Gears with Viral Video (3.5M+ Views!)",
       image: "blog-1-2.jpg",
       admin: "admin",
